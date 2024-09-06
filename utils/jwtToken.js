fa825f9ce6e45376f78a1d7c410c0a6ed3e7d24c
sarang lohe <saranglohe1999@gmail.com>
2024-09-07 01:32:04 +0530

    commit

---
+++ utils/jwtToken.js
@@ -0,0 +1,22 @@
+// require('dotenv').config()
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const sendToken = (user, statusCode, res, message) => {
+  const token = user.getJWTToken();
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true, 
+    secure: true,
+    sameSite:'none'
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    message,
+    token,
+  });
+};
